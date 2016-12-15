d9ed7f9868da45bd059828915aefc4e923fe592a
Alex Giacobbe & Alexandra Cooper & Emily Gottlieb & Lijie Yang <pair+AlexGio18.babiesinspace.egott.jessieyang0320@devbootcamp.com>
2016-12-15 17:25:53 -0500

    Refactor seed data. Refactor vote tabele. Write method to easily find points for comments/answers/question

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,9 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
   has_many :votes, as: :votable
   validates_presence_of :body
+
+  def vote_value
+    self.votes.map {|vote| vote.points }.reduce(:+)
+  end
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,9 @@ class Question < ActiveRecord::Base
   belongs_to :user
   has_many :votes, as: :votable
   validates_presence_of :title, :body
+
+  def vote_value
+    self.votes.map {|vote| vote.points }.reduce(:+)
+  end
+  
 end
