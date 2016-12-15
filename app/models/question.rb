2d17738b9ee9ccb2d29f8b9c6d7cc2977cf7c632
Alex Giacobbe & Alexandra Cooper & Emily Gottlieb & Lijie Yang <pair+AlexGio18.babiesinspace.egott.jessieyang0320@devbootcamp.com>
2016-12-15 17:56:56 -0500

    Fix question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to :user
+  has_many :answers
+  has_many :comments 
   has_many :votes, as: :votable
   validates_presence_of :title, :body
 
   def vote_value
     self.votes.map {|vote| vote.points }.reduce(:+)
   end
-  
+
 end
