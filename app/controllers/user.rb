1429617b4c5f1ad3d24069e861c5f2fb007b334f
Emily Gottlieb <email": null>
2016-12-16 15:06:26 -0500

    js error

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,20 +1,17 @@
-get '/users/new' do 
+get '/users/new' do
 	erb :'users/new'
 
-end 
+end
 
 post '/users/new' do 
-
 	@user = User.new(params[:user])
 
-	if @user.save 
-		session[:user_id] = @user.id 
+	if @user.save
+		session[:user_id] = @user.id
 		redirect '/questions'
-	else 
+	else
 		@errors=@user.errors.full_messages
 		# note it`s plural, messages
 	    erb :'users/new'
-	end 
-end 
-
-
+	end
+end
