cd93f34b6c4710d2217c64fffc9d1f4f9b0a5d09
Alex Giacobbe <email": null>
2016-12-16 17:21:13 -0500

    making user authentication

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -5,12 +5,30 @@ end
 
 post '/questions/:question_id/answers' do
   question = Question.find(params[:question_id])
-  @answer = Answer.new(body: params[:answer], question_id: "#{question.id}", user_id: "#{question.user_id}")
+  @answer = Answer.new(body: params[:answer][:body], question_id: "#{question.id}", user_id: "#{question.user_id}")
 
   if @answer.save
     redirect "/questions/#{question.id}"
   else
     @errors = @answer.errors.full_messages
-    erb :"/questions/#{question.id}/answers/new"
+    erb :"/answers/new"
   end
+end
+
+get '/answers/:answer_id/edit' do
+   @answer = Answer.find(params[:answer_id])
+   @question = @answer.question
+
+  if current_user
+    erb :'answers/edit'
+  else
+    redirect "/questions/#{@question.id}"
+  end
+end
+
+delete '/answers/:answer_id' do
+  @answer = Answer.find(params[:answer_id])
+  @question = @answer.question
+  @answer.destroy
+  redirect "/questions/#{@question.id}"
 end
